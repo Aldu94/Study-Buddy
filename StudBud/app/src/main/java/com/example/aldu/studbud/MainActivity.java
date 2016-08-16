59c2888e7ed7e52194b48fd38c164290f8311f77
Dominik Altmann <dominik.a94@gmx.de>
2016-08-16 13:07:28 +0200

    test commit

--- StudBud/app/src/main/java/com/example/aldu/studbud/MainActivity.java
+++ StudBud/app/src/main/java/com/example/aldu/studbud/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button notenRechner;
     private Button profil;
     private Button einstellungen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
