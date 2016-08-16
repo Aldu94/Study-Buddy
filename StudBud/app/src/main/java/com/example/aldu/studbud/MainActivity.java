94e04cbe2c599196efb0ea56f60330ca2d73ad65
Dominik Altmann <dominik.a94@gmx.de>
2016-08-16 13:45:27 +0200

    renamed

--- StudBud/app/src/main/java/com/example/aldu/studbud/MainActivity.java
+++ StudBud/app/src/main/java/com/example/aldu/studbud/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.aldu.studbud;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,64 +10,64 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button stundenPlan;
-    private Button notenRechner;
-    private Button profil;
-    private Button einstellungen;
+    private Button scheduleButton;
+    private Button calculatorButton;
+    private Button profileButton;
+    private Button preferencesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupUI();
-        onStundenplanClicked();
-        onNotenrechnerClicked();
-        onProfilClicked();
+        onScheduleClicked();
+        onCalculatorClicked();
+        onProfileClicked();
         onPreferencesClicked();
 
     }
     // setup buttons on main screen
     private void setupUI(){
-        stundenPlan = (Button)findViewById(R.id.stundenplan_button);
-        notenRechner = (Button)findViewById(R.id.notenrechner_button);
-        profil = (Button)findViewById(R.id.profil_button);
-        einstellungen = (Button)findViewById(R.id.einstellungen_button);
+        scheduleButton = (Button)findViewById(R.id.schedule_button);
+        calculatorButton = (Button)findViewById(R.id.calculator_button);
+        profileButton = (Button)findViewById(R.id.profile_button);
+        preferencesButton = (Button)findViewById(R.id.preferences_button);
     }
 
-    private void onStundenplanClicked(){
-        stundenPlan.setOnClickListener(new View.OnClickListener() {
+    private void onScheduleClicked(){
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,Stundenplan.class);
+                Intent i = new Intent(MainActivity.this, Schedule.class);
                 startActivity(i);
             }
         });
     }
 
-    private void onNotenrechnerClicked(){
-        notenRechner.setOnClickListener(new View.OnClickListener() {
+    private void onCalculatorClicked(){
+        calculatorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nrIntent = new Intent(MainActivity.this,Notenrechner.class);
+                Intent nrIntent = new Intent(MainActivity.this, MarksCalculator.class);
                 startActivity(nrIntent);
             }
         });
     }
 
-    private void onProfilClicked(){
-        profil.setOnClickListener(new View.OnClickListener() {
+    private void onProfileClicked(){
+        profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent profilIntent = new Intent(MainActivity.this,Profil.class);
+                Intent profilIntent = new Intent(MainActivity.this, Profile.class);
                 startActivity(profilIntent);
             }
         });
     }
 
     private void onPreferencesClicked(){
-        einstellungen.setOnClickListener(new View.OnClickListener() {
+        preferencesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent einstellungenIntent = new Intent(MainActivity.this,Preferences.class);
+                Intent einstellungenIntent = new Intent(MainActivity.this, Preferences.class);
                 startActivity(einstellungenIntent);
             }
         });
--- StudBud/app/src/main/java/com/example/aldu/studbud/MarksCalculator.java
+++ StudBud/app/src/main/java/com/example/aldu/studbud/MarksCalculator.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Notenrechner extends AppCompatActivity {
+public class MarksCalculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notenrechner);
+        setContentView(R.layout.activity_markscalculator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
