762746635d0b93ba55e9ca54faeb41fae856a19b
Dominik Altmann <Aldu@dhcp193162.uni-regensburg.de>
2016-08-31 13:42:26 +0200

    new Calculator class for calculating marks

--- StudBud/app/src/main/java/com/example/aldu/studbud/MarksCalculator.java
+++ StudBud/app/src/main/java/com/example/aldu/studbud/MarksCalculator.java
@@ -1,11 +1,14 @@
 package com.example.aldu.studbud;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.text.DecimalFormat;
@@ -13,177 +16,33 @@ import java.text.DecimalFormat;
 public class MarksCalculator extends AppCompatActivity {
 
 
-    private static final int INFWISS_ID = 0;
-    private static final int MEDINFO_ID = 1;
-    private static final String KEY_CHOSEN_MODULE_4 = "INF4";
-    private static final String KEY_CHOSEN_MODULE_5 = "INF5";
-    private static final String KEY_CHOSEN_MODULE_6 = "INF6";
     private int subjectID;
     private User user;
-
-    private Database db = new Database(MarksCalculator.this);
-
-
-
-
+    private Calculator calculator;
+    private double markOne;
+    private EditText editTextOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_markscalculator);
-        user = new User("Dominik",0,6);
+        setupUI();
+        user = new User("Karl",0,3);
         subjectID = user.getMainSubjectID();
-        calculateMarks();
-    }
-
-    private void calculateMarks(){
-        if(subjectID == INFWISS_ID){
-            calculateInfwissAsMainSubject();
-        }
-        else{
-            calculateMedInfoAsMainSubject();
-        }
-    }
-
-
-    private void calculateInfwissAsMainSubject(){
-        DecimalFormat df = new DecimalFormat("#0.0");
-        double markBachelorInfWiss = 4.0;
-
-        double inf1 = calculateInfwissM01() * 0.1;
-        double inf2 = calculateInfwissM02() * 0.1;
-        double med10 = calculateMediInfoM10() * 0.1;
-        double inf4 = calculateInfwissM04() * 0.15;
-        double inf5 = calculateInfwissM05() * 0.15;
-        double inf6 = calculateInfwissM06() * 0.15;
-        double inf7 = calculateInfwissM07() * 0.25;
-
-        double markMedInfo = calculateMedInfoAsSecondSubject() * 0.3;
-        double markInfwiss = (inf1 + inf2 + med10 + inf4 + inf5 + inf6 + inf7) * 0.5;
-
-        double finalMark = markInfwiss + markMedInfo + (markBachelorInfWiss * 0.2);
-
-
-        Toast.makeText(MarksCalculator.this, df.format(finalMark), Toast.LENGTH_SHORT).show();
-    }
-
-    private void calculateMedInfoAsMainSubject() {
-        DecimalFormat df = new DecimalFormat("#0.0");
-        double markBachelorMedInfo = 4.0;
-
-        double med3 = calculateMediInfoM03() * 0.25;
-        double med4 = calculateMediInfoM04() * 0.25;
-        double med5 = calculateMediInfoM05() * 0.25;
-        double med10 = calculateMediInfoM10() * 0.25;
-
-        double markMedInfo = (med3 + med4 + med5 + med10) * 0.5;
-        double markInfWiss = calculateInfWissAsSecondSubject();
-
-        double finalMark = markMedInfo + markInfWiss + (markBachelorMedInfo * 0.2);
-
-
-        Toast.makeText(MarksCalculator.this, df.format(finalMark), Toast.LENGTH_SHORT).show();
-    }
-
-    private double calculateInfWissAsSecondSubject(){
-        String chosenModuleOne = "INF4";
-        String chosenModuleTwo = "INF5";
-        double inf1 = calculateInfwissM01() * 0.25;
-        double inf2 = calculateInfwissM02() * 0.25;
-        double chosenModuleInf4 = calculateInfwissM04() * 0.25;
-        double chosenModuleInf5 = calculateInfwissM05() * 0.25;
-        double chosenModuleInf6 = calculateInfwissM06() * 0.25;
-
-        double finalMark = inf1 + inf2;
-
-        switch (chosenModuleOne){
-            case KEY_CHOSEN_MODULE_4:
-                finalMark += chosenModuleInf4;
-                break;
-            case KEY_CHOSEN_MODULE_5:
-                finalMark += chosenModuleInf5;
-                break;
-            case KEY_CHOSEN_MODULE_6:
-                finalMark += chosenModuleInf6;
-                break;
-            default:
-                break;
-        }
-
-        switch (chosenModuleTwo){
-            case KEY_CHOSEN_MODULE_4:
-                finalMark += chosenModuleInf4;
-                break;
-            case KEY_CHOSEN_MODULE_5:
-                finalMark += chosenModuleInf5;
-                break;
-            case KEY_CHOSEN_MODULE_6:
-                finalMark += chosenModuleInf6;
-                break;
-            default:
-                break;
-        }
-
-        return finalMark * 0.3;
-    }
-
-    private double calculateMedInfoAsSecondSubject(){
-        double med1 = calculateMediInfoM01() * 0.25;
-        double med3 = calculateMediInfoM03() * 0.25;
-        double med5 = calculateMediInfoM05() * 0.25;
-        double med8 = calculateMediInfoM08() * 0.25;
-        double mark = med1 + med3 + med5 + med8;
-        return mark;
-    }
-
-    private double calculateInfwissM01(){
-        return 3.0;
-    }
-
-    private double calculateInfwissM02(){
-        return 2.5;
-    }
-
-    private double calculateInfwissM04(){
-        return 2.65;
-    }
-
-    private double calculateInfwissM05(){
-        return 1.5;
-    }
-
-    private double calculateInfwissM06(){
-        return 2.325;
-    }
-
-    private double calculateInfwissM07(){
-        return 1.0;
-    }
-
-    private double calculateMediInfoM01(){
-        return 2.79;
-    }
-
-
-
-    private double calculateMediInfoM03(){
-        return 3.325;
-    }
-
-    private double calculateMediInfoM04(){
-        return 4.0;
-    }
-
-    private double calculateMediInfoM05(){
-        return 3.325;
+        calculator = new Calculator();
+        calculator.calculateMarks(subjectID);
 
     }
 
-    private double calculateMediInfoM08(){
-        return 2.85;
+    private void setupUI(){
+        editTextOne = (EditText)findViewById(R.id.mark1);
+        markOne = Double.parseDouble(editTextOne.getText().toString());
     }
 
-    private double calculateMediInfoM10(){
-        return 1.0;
+    private void setMarksToCalculate(){
+        Intent i = new Intent(MarksCalculator.this,Calculator.class);
+        i.putExtra("markOne",markOne);
+        startService(i);
     }
+
 }
